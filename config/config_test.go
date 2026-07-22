9f7c092d1b780704479866b103f647065a5ea5d6
vitaliy.kravchenko <vkravchenko@playwire.com>
2026-07-22 18:45:58 +0300

    Enable GZIP compression

--- config/config_test.go
+++ config/config_test.go
@@ -204,8 +204,8 @@ func TestDefaults(t *testing.T) {
 	cmpInts(t, "price_floors.fetcher.max_retries", 10, cfg.PriceFloors.Fetcher.MaxRetries)
 
 	// Assert compression related defaults
-	cmpBools(t, "compression.request.enable_gzip", false, cfg.Compression.Request.GZIP)
-	cmpBools(t, "compression.response.enable_gzip", false, cfg.Compression.Response.GZIP)
+	cmpBools(t, "compression.request.enable_gzip", true, cfg.Compression.Request.GZIP)
+	cmpBools(t, "compression.response.enable_gzip", true, cfg.Compression.Response.GZIP)
 
 	cmpBools(t, "account_defaults.price_floors.enabled", false, cfg.AccountDefaults.PriceFloors.Enabled)
 	cmpInts(t, "account_defaults.price_floors.enforce_floors_rate", 100, cfg.AccountDefaults.PriceFloors.EnforceFloorsRate)
